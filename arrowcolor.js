ea1837ef23e3ed4beb933b0e97d52519e0a47f02
austinplambeck <90013723+austinplambeck@users.noreply.github.com>
2022-05-08 19:15:00 -0700

    version 2.0 // includes desktop

--- arrowcolor.js
+++ arrowcolor.js
@@ -1,4 +1,6 @@
 // changing color on hover for the arrows
+
+// div containers for arrows and next section title
 let toRight = document.querySelector('.toRight');
 let toRight2 = document.querySelector('.toRight2');
 let toRight3 = document.querySelector('.toRight3');
@@ -6,27 +8,32 @@ let toLeft = document.querySelector('.toLeft');
 let toLeft2 = document.querySelector('.toLeft2');
 let toLeft3 = document.querySelector('.toLeft3');
 
+// arrows themselves
 let rightArrow1 = document.getElementById('rightArrow1');
 let rightArrow2 = document.getElementById('rightArrow2');
+let rightArrow2Two = document.getElementById('rightArrow2Two');
+
 let leftArrow = document.getElementById('leftArrow');
+let leftArrowTwo = document.getElementById('leftArrowTwo');
+let leftArrowThree = document.getElementById('leftArrowThree');
+
+// next section titles, under arrows
 let rightArrow1Title = document.getElementById('rightArrow1Title');
 let rightArrow2Title = document.getElementById('rightArrow2Title');
-let leftArrowTitle = document.getElementById('leftArrowTitle');
+let rightArrow2TitleTwo = document.getElementById('rightArrow2TitleTwo');
 
-let leftArrowTwo = document.getElementById('leftArrowTwo');
+let leftArrowTitle = document.getElementById('leftArrowTitle');
 let leftArrowTitleTwo = document.getElementById('leftArrowTitleTwo');
-let leftArrowThree = document.getElementById('leftArrowThree');
 let leftArrowTitleThree = document.getElementById('leftArrowTitleThree');
 
-let rightArrow2Two = document.getElementById('rightArrow2Two');
-let rightArrow2TitleTwo = document.getElementById('rightArrow2TitleTwo');
+
+// RIGHT ARROWS
 
 // right arrow 1
 toRight.addEventListener('mouseover', () => {
     rightArrow1.style.color = 'yellow';
     rightArrow1Title.style.color = 'yellow';
 })
-
 toRight.addEventListener('mouseout', () => {
     rightArrow1.style.color = '#E7E5FF';
     rightArrow1Title.style.color = '#E7E5FF';
@@ -38,63 +45,50 @@ toRight2.addEventListener('mouseover', () => {
     rightArrow2Title.style.color = 'yellow';
 
 })
-
 toRight2.addEventListener('mouseout', () => {
     rightArrow2.style.color = '#E7E5FF';
     rightArrow2Title.style.color = '#E7E5FF';
 
 })
 
-
+// right arrow 3
 toRight3.addEventListener('mouseover', () => {
 
     rightArrow2Two.style.color = 'yellow';
     rightArrow2TitleTwo.style.color = 'yellow';
 })
-
 toRight3.addEventListener('mouseout', () => {
 
     rightArrow2Two.style.color = '#E7E5FF';
     rightArrow2TitleTwo.style.color = '#E7E5FF';
 })
 
-
-
+// LEFT ARROWS
 
 // left arrow
 toLeft.addEventListener('mouseover', () => {
     leftArrow.style.color = 'yellow';
     leftArrowTitle.style.color = 'yellow';
-    leftArrowTwo.style.color = 'yellow';
-    leftArrowTitleTwo.style.color = 'yellow';
-    leftArrowThree.style.color = 'yellow';
-    leftArrowTitleThree.style.color = 'yellow';
-})
 
+})
 toLeft.addEventListener('mouseout', () => {
     leftArrow.style.color = '#E7E5FF';
     leftArrowTitle.style.color = '#E7E5FF';
-    leftArrowTwo.style.color = '#E7E5FF';
-    leftArrowTitleTwo.style.color = '#E7E5FF';
-    leftArrowThree.style.color = '#E7E5FF';
-    leftArrowTitleThree.style.color = '#E7E5FF';
+
 })
 
+// left arrow 2
 toLeft2.addEventListener('mouseover', () => {
     leftArrowTwo.style.color = 'yellow';
     leftArrowTitleTwo.style.color = 'yellow';
 
 })
-
 toLeft2.addEventListener('mouseout', () => {
     leftArrowTwo.style.color = '#E7E5FF';
     leftArrowTitleTwo.style.color = '#E7E5FF';
 })
 
-
-
-
-
+// left arrow 3
 toLeft3.addEventListener('mouseover', () => {
 
     leftArrowThree.style.color = 'yellow';
--- collapsible.js
+++ collapsible.js
@@ -9,13 +9,18 @@ let indentedLI2 = document.getElementById('indentedLI2');
 let indentedLI3 = document.getElementById('indentedLI3');
 let indentedLI4 = document.getElementById('indentedLI4');
 
+//colored arrows
+let yellowArrow = document.getElementById('yellowArrow');
+
 const collapse1 = () => {
     if (indentedLI1.style.display === 'none') {
         indentedLI1.style.display = 'list-item';
         expTitle1.innerHTML = 'Freelance Web Developer (Mar. 2022 - ) &#9650;';
+        expTitle1.style.color = 'yellow';
     } else {
         indentedLI1.style.display = 'none';
-        expTitle1.innerHTML = 'Freelance Web Developer (Mar. 2022 - ) &#9660;'
+        expTitle1.innerHTML = 'Freelance Web Developer (Mar. 2022 - ) <span id="yellowArrow">&#9660;</span>';
+        expTitle1.style.color = '#E7E5FF';
     }
 }
 
@@ -24,10 +29,12 @@ const collapse2 = () => {
         indentedLI2.style.display = 'list-item';
         indentedLI3.style.display = 'list-item';
         expTitle2.innerHTML = 'Author (Oct. 2018 - ) &#9650;';
+        expTitle2.style.color = 'yellow';
     } else {
         indentedLI2.style.display = 'none';
         indentedLI3.style.display = 'none';
-        expTitle2.innerHTML = 'Author (Oct. 2018 - ) &#9660;'
+        expTitle2.innerHTML = 'Author (Oct. 2018 - ) <span id="yellowArrow">&#9660;</span>';
+        expTitle2.style.color = '#E7E5FF';
     }
 }
 
@@ -35,8 +42,10 @@ const collapse3 = () => {
     if (indentedLI4.style.display === 'none') {
         indentedLI4.style.display = 'list-item';
         expTitle3.innerHTML = 'Tutor (Feb. 2020 - Nov. 2021) &#9650;';
+        expTitle3.style.color = 'yellow';
     } else {
         indentedLI4.style.display = 'none';
-        expTitle3.innerHTML = 'Tutor (Feb. 2020 - Nov. 2021) &#9660;'
+        expTitle3.innerHTML = 'Tutor (Feb. 2020 - Nov. 2021) <span id="yellowArrow">&#9660;</span>'
+        expTitle3.style.color = '#E7E5FF';
     }
 }
