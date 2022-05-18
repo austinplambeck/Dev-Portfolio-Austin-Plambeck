ec7e560a2866352ae7739af92c0eae80ed3c4b72
austinplambeck <90013723+austinplambeck@users.noreply.github.com>
2022-05-18 16:56:41 -0700

    version 2.12 // About arrow and font color

--- collapsible.js
+++ collapsible.js
@@ -15,12 +15,11 @@ let yellowArrow = document.getElementById('yellowArrow');
 const collapse1 = () => {
     if (indentedLI1.style.display === 'none') {
         indentedLI1.style.display = 'list-item';
-        expTitle1.innerHTML = 'Freelance Web Developer (Mar. 2022 - ) &#9650;';
-        expTitle1.style.color = 'yellow';
+        expTitle1.innerHTML = 'Freelance Web Developer (Mar. 2022 - ) <span id="yellowArrow">&#9650;</span>';
+        
     } else {
         indentedLI1.style.display = 'none';
         expTitle1.innerHTML = 'Freelance Web Developer (Mar. 2022 - ) <span id="yellowArrow">&#9660;</span>';
-        expTitle1.style.color = '#E7E5FF';
     }
 }
 
@@ -28,24 +27,20 @@ const collapse2 = () => {
     if (indentedLI2.style.display === 'none' && indentedLI3.style.display === 'none') {
         indentedLI2.style.display = 'list-item';
         indentedLI3.style.display = 'list-item';
-        expTitle2.innerHTML = 'Author (Oct. 2018 - ) &#9650;';
-        expTitle2.style.color = 'yellow';
+        expTitle2.innerHTML = 'Author (Oct. 2018 - ) <span id="yellowArrow">&#9650;</span>';
     } else {
         indentedLI2.style.display = 'none';
         indentedLI3.style.display = 'none';
         expTitle2.innerHTML = 'Author (Oct. 2018 - ) <span id="yellowArrow">&#9660;</span>';
-        expTitle2.style.color = '#E7E5FF';
     }
 }
 
 const collapse3 = () => {
     if (indentedLI4.style.display === 'none') {
         indentedLI4.style.display = 'list-item';
-        expTitle3.innerHTML = 'Tutor (Feb. 2020 - Nov. 2021) &#9650;';
-        expTitle3.style.color = 'yellow';
+        expTitle3.innerHTML = 'Tutor (Feb. 2020 - Nov. 2021) <span id="yellowArrow">&#9650;</span>';
     } else {
         indentedLI4.style.display = 'none';
         expTitle3.innerHTML = 'Tutor (Feb. 2020 - Nov. 2021) <span id="yellowArrow">&#9660;</span>'
-        expTitle3.style.color = '#E7E5FF';
     }
 }
